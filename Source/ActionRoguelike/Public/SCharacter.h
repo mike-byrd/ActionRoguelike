f3a4f24b980516350d29ca113d2b6a735b0674fb
Mike <michaelcbyrdjr@gmail.com>
2022-04-16 16:32:56 -0700

    Added SCharacter & Setup Input
    - Added character class
    - Added MoveForward/Turn input

---
+++ Source/ActionRoguelike/Public/SCharacter.h
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "SCharacter.generated.h"
+
+class UCameraComponent;
+class USpringArmComponent;
+
+UCLASS()
+class ACTIONROGUELIKE_API ASCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ASCharacter();
+
+protected:
+	
+	UPROPERTY(VisibleAnywhere)
+	UCameraComponent* CameraComp;
+
+	// Spring Arm checks for collision and adjusts it's position so it's view isn't obstructed.
+	UPROPERTY(VisibleAnywhere)
+	USpringArmComponent* SpringArmComp;
+	
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	void MoveForward(float Value);
+
+};
